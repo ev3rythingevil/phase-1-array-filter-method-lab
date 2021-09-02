9ce22ec96e9cfda74011610c1c81d1c6ef7963ec
Hathor Hamilton <koshknot@gmail.com>
2021-09-02 18:39:15 -0400

    commit tree a6d18b05d23d654f2639a4c610fecfc4e6b4b419

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(names , string){
+    //takes an array of names and a string
+    // returns matching list of drivers
+    // .filter takes a callback function and runs it through the array
+    // if the return is true, the element is copied to a new array
+const nameFilter = names.filter(function(names){
+    return names.toLowerCase() === string.toLowerCase();
+  });
+  return nameFilter;
+}
+
+function fuzzyMatch(names,string){
+    //returns drivers name that matches provided letters
+const nameFilter = names.filter(function(el){
+          return el[0] === string[0];
+ });
+ return nameFilter;
+}
+
+function matchName(driver,string){
+    //driver has two properties: name and hometown
+    //return each element whose name === string
+    // return the whole object
+    const nameMatch = driver.filter((name)=> {
+        return name === string
+         });
+    return nameMatch.keys;
+}
